559fe6b02dd2112332c086b083d3c5ac5ae24ac3
Emirhan Gök <Emirhan Gk>
2026-06-22 22:45:07 +0300

    Initial Commit

---
+++ System Info/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  System Info
+//
+//  Created by Emirhan Gök on 22.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ System Info/System_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  System_InfoApp.swift
+//  System Info
+//
+//  Created by Emirhan Gök on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct System_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
